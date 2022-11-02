a7c5796bc39af37e52b09e1c1ef17425620d0d15
Atul0901 <85149350+Atul0901@users.noreply.github.com>
2022-11-02 19:06:54 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ const route = require("./routes/route.js");
 const mongoose = require("mongoose");
 const app = express();
 const multer = require("multer");
-const { response } = require("express");
-// 1const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
 app.use(multer().any())
-app.set('port', (process.env.PORT||5000))
+// app.set('port', (process.env.PORT||5000))
 
 mongoose
     .connect(
@@ -22,20 +21,19 @@ mongoose
 
 app.use("/", route);
 
-app.get('/',function(request , reesponse){
-    var result ='app is running'
-    response.send(result);
-}).listen(app.get('port'),function(){
-    console.log('App is running ', 'server is listening on port ' , app.get('port'));
-})
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
 
-
-
+// app.get('/',function(request , reesponse){
+//     var result ='app is running'
+//     response.send(result);
+// }).listen(app.get('port'),function(){
+//     console.log('App is running ', 'server is listening on port ' , app.get('port'));
+// })
 
 // app.listen(process.env.PORT || 3000, function () {
 //     console.log(chalk.blue("Express app running on port " PORT);
 // });
 
-// app.listen(PORT, () => {
-//     console.log(`server started on port ${PORT}`);
-// });
+//mongodb+srv://Atul_0901:Hfwt2iFlfIJLT4Kt@cluster0.7iymhpk.mongodb.net/preinterview1
